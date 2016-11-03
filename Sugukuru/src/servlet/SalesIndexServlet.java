8c632056b63cfa74c2b1bd41b25fbf6045b43ed9
KokiKono <61747641koki@gmail.com>
2016-11-03 20:47:36 +0900

    no message

--- Sugukuru/src/servlet/SalesIndexServlet.java
+++ Sugukuru/src/servlet/SalesIndexServlet.java
@@ -1,15 +1,12 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import beans.InspectionValue;
 
 /**
  * Servlet implementation class SalesIndexServlet
@@ -40,8 +37,6 @@ public class SalesIndexServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println(InspectionValue.readSql(this, "test.sql"));
 
 
 	}
