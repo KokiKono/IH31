92955187091e9947a03c0f54cbc598194f7ef696
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 18:44:49 +0900

    add Lone.java (貸出)　変数追加

--- Sugukuru/src/dtd/Lone.java
+++ Sugukuru/src/dtd/Lone.java
@@ -6,6 +6,48 @@
  * *************************/
 package dtd;
 
-public class Lone {
+import beans.CalendarByKoki;
 
+public class Lone {
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param loneId int 貸し出しID
+	 */
+	public int loneId;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param customerId String 顧客ID
+	 */
+	public String customerId;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param address String 顧客住所
+	 */
+	public String address;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param phoneNomber String 電話番号
+	 */
+	public String phoneNomber;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param loneStartDate CalendarByKoki 貸出日
+	 */
+	public CalendarByKoki loneStartDate;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param loneEndDate CalendarByKoki 返却日
+	 */
+	public CalendarByKoki loneEndDate;
 }
