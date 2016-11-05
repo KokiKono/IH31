2bcfd045183c117d8fdcff14d515a79786e99caa
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 17:00:10 +0900

    add Inventories.java (棚卸し) 変数追加

--- Sugukuru/src/dtd/Inventories.java
+++ Sugukuru/src/dtd/Inventories.java
@@ -7,5 +7,45 @@
 package dtd;
 
 public class Inventories {
-
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param inventoriesId in 棚卸ID
+	 */
+	public int inventoriesId;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param puoductId String 商品ID
+	 */
+	public String puoductId;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param puroductName String 商品名
+	 */
+	public String puroductName;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param inventoriesAmount int 棚卸数
+	 */
+	public int inventoriesAmount;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param finishedAmount int 完了数
+	 */
+	public int finishedAmount;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param note String　備考
+	 */
+	public String note;
 }
