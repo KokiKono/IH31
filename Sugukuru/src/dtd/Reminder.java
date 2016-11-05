a60c47c470ed224f6a867a243fa9ec29276de619
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 17:03:54 +0900

    add Reminder.java (督促状)　変数追加

--- Sugukuru/src/dtd/Reminder.java
+++ Sugukuru/src/dtd/Reminder.java
@@ -6,6 +6,41 @@
  * *************************/
 package dtd;
 
-public class Reminder {
+import beans.CalendarByKoki;
 
+public class Reminder {
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param settlementID int 決済ID
+	 */
+	public int settlementID;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param num int 行番号
+	 */
+	public int num;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param dunningDate CalendarByKoki 督促状送付日
+	 */
+	public CalendarByKoki dunningDate;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param dunningFlg int 督促状送付フラグ
+	 */
+	public int dunningFlg;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param note String 備考
+	 */
+	public String note;
 }
