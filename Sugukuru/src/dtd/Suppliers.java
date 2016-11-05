6f33ff8d41adec81c303a9ab58d47fdc8f92edeb
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 18:58:27 +0900

    add Suppliers.java (取引先) 変数追加

--- Sugukuru/src/dtd/Suppliers.java
+++ Sugukuru/src/dtd/Suppliers.java
@@ -7,5 +7,52 @@
 package dtd;
 
 public class Suppliers {
-
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param suppliersId int 取引先ID
+	 */
+	public int suppliersId;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param customerFlg int 法人顧客フラグ
+	 */
+	public int customerFlg;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param makerFlg int メーカーフラグ
+	 */
+	public int makerFlg;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param postalCode String 郵便番号
+	 */
+	public String postalCode;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param prefectures String 都道府県
+	 */
+	public String prefectures;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param address String 住所
+	 */
+	public String address;
+	
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param phoneNumber String 電話番号
+	 */
+	public String phoneNumber;
 }
