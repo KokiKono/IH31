4f45b552aaa512942377c7c94c022a697e8d372c
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 21:29:05 +0900

    add SugkuruAndroid プロジェクト追加

---
+++ SugukuruAndroid/app/src/main/java/local/hal/st32/android/sugukuruandroid/SugukuruActivity.java
@@ -0,0 +1,13 @@
+package local.hal.st32.android.sugukuruandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SugukuruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sugukuru);
+    }
+}
