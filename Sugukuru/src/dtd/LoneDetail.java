40b94ff44ab62657a656172eb42d2f6b2ea7afbd
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-05 19:51:14 +0900

    add LoneDetail.java(貸出明細) 変数追加

--- Sugukuru/src/dtd/LoneDetail.java
+++ Sugukuru/src/dtd/LoneDetail.java
@@ -7,5 +7,22 @@
 package dtd;
 
 public class LoneDetail {
-	public int 
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param loneId int 貸出ID
+	 */
+	public int loneId;
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param sampleId String サンプルID
+	 */
+	public String sampleId;
+	/**
+	 * @auther Tester
+	 * 2016/11/05
+	 * @param loneAmount int 貸出数量
+	 */
+	public int loneAmount;
 }
