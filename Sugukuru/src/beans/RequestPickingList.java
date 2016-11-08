dbf7ab3c1fe7fe93a04c2026c863d0eaa2749482
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-08 15:59:27 +0900

    a

--- Sugukuru/src/beans/RequestPickingList.java
+++ Sugukuru/src/beans/RequestPickingList.java
@@ -2,10 +2,30 @@
  * 学籍番号:40024
  * 作成者　:t.ueshima
  * 作成日　:2016/11/07
- * 内容　　:
+ * 内容　　:AndroidからSQLを受け取りDBの情報をJSONで返す
  * *************************/
 package beans;
 
-public class RequestPickingList {
+import java.util.ArrayList;
 
+import common.Database;
+import dtd.PickingList;
+
+public class RequestPickingList {
+	private static String DBName = Database.DBName;
+	static PickingList pick = new PickingList();
+	
+	
+	public ArrayList<String> runJson(String SQL){
+		DBManager db = new DBManager(DBName);
+		
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		result = db.runSelect(SQL);
+		
+		
+		
+		db.closeDB();
+		return json;
+	}
+	
 }
