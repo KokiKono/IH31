55db60aac799ec32e7c046f78d596c7662d70d7a
KokiKono <61747641koki@gmail.com>
2016-11-09 19:43:15 +0900

    DBManager修正。

--- Sugukuru/src/servlet/StockOrderListServlet.java
+++ Sugukuru/src/servlet/StockOrderListServlet.java
@@ -63,15 +63,16 @@ public class StockOrderListServlet extends HttpServlet implements Database {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
 		Constants constants = new Constants(this, request);
-		Message message=new Message(constants);
+		Message message = new Message(constants);
 		StockOrderList stockOrderList = new StockOrderList();
 		// 検索条件の取得
 		stockOrderList = (StockOrderList) constants
 				.superDecodeRequest(stockOrderList);
 		DBManager dbManager = null;
+		PreparedStatementByKoki statementByKoki=null;
 		try {
 			dbManager = new DBManager(DBName);
-			PreparedStatementByKoki statementByKoki = dbManager
+			statementByKoki = dbManager
 					.getStatementByKoki(InspectionValue.readSql(this,
 							"StockOrderList.sql"));
 			// /////////////////
@@ -81,46 +82,65 @@ public class StockOrderListServlet extends HttpServlet implements Database {
 			if (stockOrderList.rOrderId == null) {
 				// 受注ID取得不能
 				statementByKoki.toNull("ORDER_ID");
-			} else {
+			} else if (stockOrderList.rOrderId.isEmpty() == false) {
 				// 受注ID不正値
 				try {
 					statementByKoki.setInt("ORDER_ID",
 							Integer.parseInt(stockOrderList.rOrderId));
 				} catch (NumberFormatException e) {
-					message.doWarnig("02","08","08");
+					message.doWarnig("02", "08", "08");
 					statementByKoki.toNull("ORDER_ID");
 				}
+			}else{
+				//受注IDが空白
+				statementByKoki.toNull("ORDER_ID");
 			}
 			// 顧客ID
 			if (stockOrderList.customerId == null) {
 				// 顧客ID取得ID
 				statementByKoki.toNull("CUSTOMER_ID");
-			} else {
+			} else if (stockOrderList.customerId.isEmpty() == false) {
 				try {
 					Integer.parseInt(stockOrderList.customerId);
 					statementByKoki.setString("CUSTOMER_ID",
 							stockOrderList.customerId);
 				} catch (NumberFormatException e) {
-					//顧客ID不整値
+					// 顧客ID不整値
 					e.printStackTrace();
-					message.doWarnig("04","02");
+					message.doWarnig("04", "02");
 					statementByKoki.toNull("CUSTOMER_ID");
 				}
+			}else{
+				//顧客IDが空白
+				statementByKoki.toNull("CUSTOMER_ID");
+			}
+			if(stockOrderList.customerName==null){
+				//顧客名取得不能
+				statementByKoki.toNull("USER_NAME");
+			}else{
+				statementByKoki.setString("USER_NAME", stockOrderList.customerName+"%");
 			}
 			// 出荷日時
-			CalendarByKoki shipmentCalendar = CalendarByKoki.newInstance(
-					stockOrderList.shipmentYear, stockOrderList.shipmentMonth,
-					stockOrderList.shipmentDay);
-			if (shipmentCalendar == null) {
-				// 出荷日生成エラー
-				message.doWarnig("04","04");
+			if (stockOrderList.shipmentYear.isEmpty() == false
+					&& stockOrderList.shipmentMonth.isEmpty() == false
+					&& stockOrderList.shipmentYear.isEmpty() == false) {
+				CalendarByKoki shipmentCalendar = CalendarByKoki.newInstance(
+						stockOrderList.shipmentYear,
+						stockOrderList.shipmentMonth,
+						stockOrderList.shipmentDay);
+				if (shipmentCalendar == null) {
+					// 出荷日生成エラー
+					message.doWarnig("04", "04");
+					statementByKoki.toNull("DELIVERY_DATE");
+				} else {
+					statementByKoki.setString("DELIVERY_DATE",
+							shipmentCalendar.outSQLDate());
+				}
+			}else{
 				statementByKoki.toNull("DELIVERY_DATE");
-			} else {
-				statementByKoki.setString("DELIVERY_DATE",
-						shipmentCalendar.outSQLDate());
 			}
-			if(message.nowMode()==MODE.WARNIG){
-				//不正な値あり
+			if (message.nowMode() == MODE.WARNIG) {
+				// 不正な値あり
 				request.setAttribute("message", message);
 				constants.forward(request, response);
 				return;
@@ -136,16 +156,20 @@ public class StockOrderListServlet extends HttpServlet implements Database {
 				ArrayList<StockOrderList> stockOrderLists = new ArrayList<StockOrderList>();
 				// 検索結果を成形格納
 				try {
-					stock:for (int rowCount = 0; rowCount < list.size();) {
-						StockOrderList stockOrderList2=new StockOrderList();
+					stock: for (int rowCount = 0; rowCount < list.size();) {
+						StockOrderList stockOrderList2 = new StockOrderList();
 						stockOrderList2.orderId = Integer.parseInt(list.get(
 								rowCount).get(0));
 						stockOrderList2.customerId = list.get(rowCount).get(1);
-						stockOrderList2.customerName = list.get(rowCount).get(2);
-						stockOrderList2.orderDate=CalendarByKoki.newInstance(list.get(rowCount).get(13));
-						stockOrderList2.shipmentDate=CalendarByKoki.newInstance(list.get(rowCount).get(14));
-						stockOrderList2.deliveryDate=CalendarByKoki.newInstance(list.get(rowCount).get(10));
-						details:while (stockOrderList2.orderId == Integer
+						stockOrderList2.customerName = list.get(rowCount)
+								.get(2);
+						stockOrderList2.orderDate = CalendarByKoki
+								.newInstance(list.get(rowCount).get(13));
+						stockOrderList2.shipmentDate = CalendarByKoki
+								.newInstance(list.get(rowCount).get(14));
+						stockOrderList2.deliveryDate = CalendarByKoki
+								.newInstance(list.get(rowCount).get(10));
+						details: while (stockOrderList2.orderId == Integer
 								.parseInt(list.get(rowCount).get(0))) {
 							// 詳細を格納
 							OrderDetail detail = new OrderDetail();
@@ -166,7 +190,7 @@ public class StockOrderListServlet extends HttpServlet implements Database {
 									.get(rowCount).get(11));
 							detail.note = list.get(rowCount).get(12);
 
-							if (rowCount == list.size()-1) {
+							if (rowCount == list.size() - 1) {
 								// 最後の行処理
 								stockOrderList2.orderDetails.add(detail);
 								break details;
@@ -175,36 +199,37 @@ public class StockOrderListServlet extends HttpServlet implements Database {
 							rowCount++;
 						}
 						stockOrderLists.add(stockOrderList2);
-						if(rowCount==list.size()-1){
+						if (rowCount == list.size() - 1) {
 							break stock;
 						}
 					}
 				} catch (Exception e) {
 					// 格納失敗
 					e.printStackTrace();
-					//message.doErrer("01");
+					// message.doErrer("01");
 				}
-				if(stockOrderLists.size()>0){
-					//リクエスト格納
+				if (stockOrderLists.size() > 0) {
+					// リクエスト格納
 					request.setAttribute("stockOrderList", stockOrderLists);
 				}
 			}
 		} catch (SQLException e) {
 			// SQLエラー
 			e.printStackTrace();
-			//message.doErrer();
+			System.out.println(statementByKoki.out());
+			// message.doErrer();
 		} catch (ClassNotFoundException e) {
 			// ロードエラー
 			e.printStackTrace();
-			//message.doErrer();
+			// message.doErrer();
 		} catch (Exception e) {
 			// どうしようもないエラー
 			e.printStackTrace();
-			//message.doErrer();
-		}finally{
+			// message.doErrer();
+		} finally {
 
 		}
-//フォワード処理
-			constants.forward(request, response);
+		// フォワード処理
+		constants.forward(request, response);
 	}
 }
