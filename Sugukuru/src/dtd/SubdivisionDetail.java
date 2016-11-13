501e220056fd9febcae3f83dac9dd81a197af36e
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-14 02:09:17 +0900

    add	SubdivisionDetail.java(小分け詳細)	変数追加

--- Sugukuru/src/dtd/SubdivisionDetail.java
+++ Sugukuru/src/dtd/SubdivisionDetail.java
@@ -2,10 +2,39 @@
  * 学籍番号:40024
  * 作成者　:t.ueshima
  * 作成日　:2016/11/14
- * 内容　　:
+ * 内容　　:小分け詳細を管理する
  * *************************/
 package dtd;
 
 public class SubdivisionDetail {
-
+	/**
+	 * @auther Tester
+	 * 2016/11/14
+	 * @param num String　行番号
+	 */
+	public String num;
+	/**
+	 * @auther Tester
+	 * 2016/11/14
+	 * @param productNumber String　商品ID
+	 */
+	public String productNumber;
+	/**
+	 * @auther Tester
+	 * 2016/11/14
+	 * @param productName String　商品名
+	 */
+	public String productName;
+	/**
+	 * @auther Tester
+	 * 2016/11/14
+	 * @param Amount String　商品数量
+	 */
+	public String Amount;
+	/**
+	 * @auther Tester
+	 * 2016/11/14
+	 * @param step String　作業ステップ
+	 */
+	public String step;
 }
