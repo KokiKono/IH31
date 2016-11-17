7b1d7316836d54322d384ba64480ce52931ae5b3
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-17 14:18:04 +0900

    add AndroidPickingList.sql	whereの追加

--- Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
@@ -12,8 +12,9 @@ SELECT
       THEN 'ピッキング済'
     ELSE '未作業'
   END) AS state 
-FROM
-  working_table a
+FROM 
+  working_table a 
  INNER JOIN puroduct_master b ON a.puroduct_id = b.puroduct_id
  INNER JOIN rack_master c ON b.rack_id = c.rack_id
+ WHERE a.puroduct_id = PRODUCT_ID AND state = STATE
 ;
