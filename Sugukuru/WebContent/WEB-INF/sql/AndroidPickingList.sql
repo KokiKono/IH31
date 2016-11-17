3ae2d531082a29235e221a56ffc5707eeb311a94
ueshima <Tester@ueshimas-MacBook-Pro.local>
2016-11-17 11:15:29 +0900

    change	AndroidPickingList.sql	不要部分削除

--- Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
@@ -1,15 +1,19 @@
-SELECT a.puroduct_id, 
-  b.puroduct_name, 
-  c.rack_name, 
-  a.necessary_amount, 
-  (SELECT d.stock_amount 
-    FROM stock_table d 
-    WHERE d.puroduct_id = b.puroduct_id 
-    ORDER BY d.entry_date 
-    DESC LIMIT 1) AS stock_amount, 
-  a.picked_amount 
-FROM working_table a 
-INNER JOIN puroduct_master b ON a.puroduct_id = b.puroduct_id 
-INNER JOIN rack_master c ON b.rack_id = c.rack_id 
-ORDER BY puroduct_id
+SELECT
+  a.puroduct_id,
+  b.puroduct_name,
+  c.rack_name,
+  a.necessary_amount,
+  a.picked_amount,
+  a.inspected_amount,
+  (CASE
+    WHEN a.necessary_amount <= a.picked_amount AND a.necessary_amount <= a.inspected_amount
+      THEN '検品済'
+    WHEN a.necessary_amount <= a.picked_amount
+      THEN 'ピッキング済'
+    ELSE '未作業'
+  END) AS state 
+FROM
+  working_table a
+ INNER JOIN puroduct_master b ON a.puroduct_id = b.puroduct_id
+ INNER JOIN rack_master c ON b.rack_id = c.rack_id
 ;
