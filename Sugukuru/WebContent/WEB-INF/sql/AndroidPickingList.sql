1b9901e25b3ca32762781c4e6773b04f0ebaf723
ueshima <re-to-mikan@hotmail.co.jp>
2016-11-21 02:26:51 +0900

    add AndroidPickingList.sql	在庫数も取得できるように改良

--- Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidPickingList.sql
@@ -2,6 +2,11 @@ SELECT
   a.puroduct_id,
   b.puroduct_name,
   c.rack_name,
+  (SELECT d.stock_amount 
+    FROM stock_table d 
+    WHERE d.puroduct_id = b.puroduct_id 
+    ORDER BY d.entry_date 
+    DESC LIMIT 1) AS stock_amount, 
   a.necessary_amount,
   a.picked_amount,
   a.inspected_amount,
@@ -11,11 +16,16 @@ SELECT
     WHEN a.necessary_amount <= a.picked_amount
       THEN 'ピッキング済'
     ELSE '未作業'
-  END) AS state 
+  END) AS state, 
+  (SELECT f.amount
+  	FROM product_unit_table f
+  	WHERE a.puroduct_id = f.puroduct_id
+  	ORDER BY f.puroduct_id DESC
+  	LIMIT 1) as unit 
 FROM 
   working_table a 
  INNER JOIN puroduct_master b ON a.puroduct_id = b.puroduct_id
- INNER JOIN rack_master c ON b.rack_id = c.rack_id
+ INNER JOIN rack_master c ON b.rack_id = c.rack_id 
 /*if(PRODUCT_ID)start*/
  WHERE a.puroduct_id = PRODUCT_ID
 /*if(PRODUCT_ID)end*/
