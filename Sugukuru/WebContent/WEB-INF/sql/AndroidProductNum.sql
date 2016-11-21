33d3f8d1d3effc06859de00f6a474b45c97432a1
ueshima <re-to-mikan@hotmail.co.jp>
2016-11-21 13:55:42 +0900

    add AndroidProductNum.sql	商品数量系SQLファイル作成

---
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidProductNum.sql
@@ -0,0 +1,22 @@
+SELECT
+  a.necessary_amount,
+  a.picked_amount,
+  a.inspected_amount,
+  (
+  SELECT
+    b.stock_amount
+  FROM
+    stock_table b
+  WHERE
+    a.puroduct_id = b.puroduct_id
+  ORDER BY
+    b.entry_date DESC
+  LIMIT 1
+) stock
+FROM
+  working_table a
+WHERE
+/*if(PRODUCT_ID)start*/
+  a.puroduct_id = PRODUCT_ID
+/*if(PRODUCT_ID)end*/
+;
