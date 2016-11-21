387092b2cc390b7e938349cfc08bdd368b3e46b5
ueshima <re-to-mikan@hotmail.co.jp>
2016-11-21 13:48:34 +0900

    add ProductNums.java	変数追加

--- Sugukuru/src/dtd/ProductNums.java
+++ Sugukuru/src/dtd/ProductNums.java
@@ -7,5 +7,28 @@
 package dtd;
 
 public class ProductNums {
-
+	/**
+	 * @auther Tester
+	 * 2016/11/21
+	 * @param needs int 必要商品数
+	 */
+	public int needs;
+	/**
+	 * @auther Tester
+	 * 2016/11/21
+	 * @param pickNum int　商品ピッキング数
+	 */
+	public int pickNum;
+	/**
+	 * @auther Tester
+	 * 2016/11/21
+	 * @param inspectedAmount int　商品検品数
+	 */
+	public int inspectedAmount;
+	/**
+	 * @auther Tester
+	 * 2016/11/21
+	 * @param stock int　商品在庫数
+	 */
+	public int stock;
 }
