5621654bfae35074420f573a6c247f12873f8cb7
ueshima <re-to-mikan@hotmail.co.jp>
2016-11-22 14:49:04 +0900

    add AndroidStepUp.sql SQLファイル追加

---
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidStepUp.sql
@@ -0,0 +1,16 @@
+UPDATE
+  order_details_table a 
+INNER JOIN 
+  order_table b ON a.order_id = b.order_id 
+SET 
+/*if(STEP)start*/
+  step = STEP
+/*if(STEP)end*/
+WHERE 
+/*if(SHIPPING_DATE)start*/
+  b.shipping_schedule_date = SHIPPING_DATE 
+/*if(SHIPPING_DATE)end*/
+/*if(PRODUCT_ID)start*/
+  AND a.puroduct_id = PRODUCT_ID 
+/*if(PRODUCT_ID)end*/
+;
