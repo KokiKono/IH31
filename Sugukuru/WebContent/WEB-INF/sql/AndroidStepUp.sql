56b82509f0f578222771987a3f90b53217d8ee8b
ueshima <re-to-mikan@hotmail.co.jp>
2016-11-22 16:16:07 +0900

    change AndroidStepUp.sql	バグ修正

--- Sugukuru/WebContent/WEB-INF/sql/AndroidStepUp.sql
+++ Sugukuru/WebContent/WEB-INF/sql/AndroidStepUp.sql
@@ -1,16 +1,16 @@
-UPDATE
-  order_details_table a 
+UPDATE 
+ order_details_table a 
 INNER JOIN 
-  order_table b ON a.order_id = b.order_id 
+ order_table b ON a.order_id = b.order_id 
 SET 
-/*if(STEP)start*/
-  step = STEP
-/*if(STEP)end*/
+/*if(STEP)start*/ 
+ step = STEP 
+/*if(STEP)end*/ 
 WHERE 
-/*if(SHIPPING_DATE)start*/
-  b.shipping_schedule_date = SHIPPING_DATE 
-/*if(SHIPPING_DATE)end*/
-/*if(PRODUCT_ID)start*/
-  AND a.puroduct_id = PRODUCT_ID 
-/*if(PRODUCT_ID)end*/
+/*if(SHIPPING_DATE)start*/ 
+ b.shipping_schedule_date = SHIPPING_DATE 
+/*if(SHIPPING_DATE)end*/ 
+/*if(PRODUCT_ID)start*/ 
+ AND a.puroduct_id = PRODUCT_ID 
+/*if(PRODUCT_ID)end*/ 
 ;
